b0053bb8ea4dafdce4c43700c5a6fa4fd5d1e513
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    rewrite connection/auth process in a simpler way

--- src/helpers.jl
+++ src/helpers.jl
@@ -1,53 +1,5 @@
 using Setfield
 
-module Internal
-import ..Client, ..send_method
-
-struct Event{T}
-    data
-end
-
-step(::Client, _, ::Event) = nothing
-step(c::Client, _, e::Event{:ok}) = c.last_state = "$(c.last_state): ok"
-
-function step(c::Client, params, e::Union{Event{:updateConnectionState},Event{:updateAuthorizationState}})
-    typ = e.data[e.data["@type"] == "updateConnectionState" ? "state" : "authorization_state"]["@type"]
-    process_connecting(c, params, Event{Symbol(typ)}(e))
-    c.last_state = typ
-end
-
-process_connecting(c::Client, _, ::Event{:connectionStateConnecting}) = c.is_connected = false
-process_connecting(c::Client, _, ::Event{:connectionStateUpdating}) = c.is_connected = false
-process_connecting(c::Client, _, ::Event{:connectionStateReady}) = c.is_connected = true
-
-process_connecting(c::Client, _, ::Event{:authorizationStateClosed}) = c.is_authorized = false
-process_connecting(c::Client, _, ::Event{:authorizationStateReady}) = c.is_authorized = true
-process_connecting(c::Client, params, ::Event{:authorizationStateWaitTdlibParameters}) = send_method(
-    c,
-    :setTdlibParameters,
-    parameters=Dict(
-        "database_directory" => "tdlib",
-        "use_file_database" => false,
-        "use_chat_info_database" => false,
-        "use_message_database" => false,
-        "enable_storage_optimizer" => true,
-        "use_secret_chats" => false,
-        "api_id" => params.api_id,
-        "api_hash" => params.api_hash,
-        "system_language_code" => "en",
-        "device_model" => "Desktop",
-        "system_version" => "Linux",
-        "application_version" => "1.0",
-    )
-)
-process_connecting(c::Client, params, ::Event{:authorizationStateWaitEncryptionKey}) = send_method(c, :checkDatabaseEncryptionKey, key="")
-process_connecting(c::Client, params, ::Event{:authorizationStateWaitPhoneNumber}) = send_method(c, :setAuthenticationPhoneNumber, phone_number=params.phone_number)
-process_connecting(c::Client, params, ::Event{:authorizationStateWaitCode}) = send_method(c, :checkAuthenticationCode, code=Base.prompt("Authentication code"))
-process_connecting(c::Client, params, ::Event{:authorizationStateWaitPassword}) = send_method(c, :checkAuthenticationPassword, password=Base.prompt("Password"))
-
-end
-
-
 Base.@kwdef struct AuthParameters
     api_id::Int
     api_hash::String
@@ -56,6 +8,60 @@ end
 JSON.StructTypes.StructType(::Type{AuthParameters}) = JSON.StructTypes.Struct()
 
 handle_conn_step(client::Client, params::AuthParameters, evt::Nothing) = nothing
-handle_conn_step(client::Client, params::AuthParameters, evt) = Internal.step(client, params, Internal.Event{Symbol(evt["@type"])}(evt))
+function handle_conn_step(client::Client, params::AuthParameters, evt)
+    type = evt["@type"]
+    if type == "ok"
+        client.last_state = "$(client.last_state): ok"
+    elseif type == "updateConnectionState"
+        typ = evt["state"]["@type"]
+        if typ == "connectionStateConnecting"
+            client.is_connected = false
+        elseif typ == "connectionStateUpdating"
+            client.is_connected = false
+        elseif typ == "connectionStateReady"
+            client.is_connected = true
+        else
+            error("Unsupported connection state received: $typ")
+        end
+        client.last_state = typ
+    elseif type == "updateAuthorizationState"
+        typ = evt["authorization_state"]["@type"]
+        if typ == "authorizationStateClosed"
+            client.is_authorized = false
+        elseif typ == "authorizationStateReady"
+            client.is_authorized = true
+        elseif typ == "authorizationStateWaitTdlibParameters"
+            send_method(
+                client,
+                :setTdlibParameters,
+                parameters=Dict(
+                    "database_directory" => "tdlib",
+                    "use_file_database" => false,
+                    "use_chat_info_database" => false,
+                    "use_message_database" => false,
+                    "enable_storage_optimizer" => true,
+                    "use_secret_chats" => false,
+                    "api_id" => params.api_id,
+                    "api_hash" => params.api_hash,
+                    "system_language_code" => "en",
+                    "device_model" => "Desktop",
+                    "system_version" => "Linux",
+                    "application_version" => "1.0",
+                )
+            )
+        elseif typ == "authorizationStateWaitEncryptionKey"
+            send_method(client, :checkDatabaseEncryptionKey, key="")
+        elseif typ == "authorizationStateWaitPhoneNumber"
+            send_method(client, :setAuthenticationPhoneNumber, phone_number=params.phone_number)
+        elseif typ == "authorizationStateWaitCode"
+            send_method(client, :checkAuthenticationCode, code=Base.prompt("Authentication code"))
+        elseif typ == "authorizationStateWaitPassword"
+            send_method(client, :checkAuthenticationPassword, password=Base.prompt("Password"))
+        else
+            error("Unsupported authorization state received: $typ")
+        end
+        client.last_state = typ
+    end
+end
 
 is_ready(client::Client) = client.is_authorized && client.is_connected
