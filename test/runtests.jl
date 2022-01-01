a8d315752b0d90938ece9deaceda868997dc51a0
Alexander Plavin <alexander@plav.in>
2022-01-01 00:00:00 +0000

    add real login test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 import TelegramClient as TG
+import JSON3
 using Test
 
 
@@ -25,3 +26,18 @@ end
     TG.destroy(client)
     @test !TG.is_created(client)
 end
+
+@testset "real login" begin
+    auth_file = joinpath(@__DIR__, "auth_params.json")
+    if isfile(auth_file)
+        params = JSON3.read(read(auth_file, String), TG.AuthParameters)
+        client = TG.Client()
+        while !TG.is_ready(client)
+            TG.handle_conn_step(client, params, TG.receive(client, timeout=10))
+        end
+        @test TG.is_ready(client)
+    end
+end
+
+import CompatHelperLocal as CHL
+CHL.@check()
